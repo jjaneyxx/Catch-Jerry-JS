da9c85f3945d69a2abe419f69efed18c13cc9cdb
jjaneyxx <hmyeonge@gmail.com>
2024-08-22 17:00:54 +0900

    Feat: reset 기능, chance 를 5번으로 설정

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chanceArea = document.getElementById("chance-area");
+
+let chance = 5;
+let gameOver = false;
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -11,18 +17,37 @@ function pickRandomNum() {
 }
 
 function play() {
+  chance--;
+  chanceArea.textContent = `남은 기회 : ${chance}`;
   // console.log("게임시작");
   let userValue = userInput.value;
   if (userValue < computerNum) {
-    resultArea.textContent = "UP ⬆️";
+    resultArea.textContent = "UP !!!!! ⬆️";
     console.log("UP");
   } else if (userValue > computerNum) {
-    resultArea.textContent = "Down ⬇️";
+    resultArea.textContent = "Down !!!! ⬇️";
     console.log("DOWN");
   } else {
-    resultArea.textContent = "정답입니다🥳";
+    resultArea.textContent = "정답 !!! 🥳";
     console.log("정답");
   }
+
+  if (chance < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+    resultArea.textContent = "기회를 모두 소진함😓";
+  }
+}
+
+function reset() {
+  userInput.value = ""; // 입력했던 번호들 초기화
+  pickRandomNum(); // 새로운 번호 생성
+  resultArea.textContent = "게임을 다시 시작함";
+  chance = 5;
+  chanceArea.textContent = "기회가 5번 있음";
 }
 
 pickRandomNum();
