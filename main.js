cee9da2c850ad5109b3aef6e77321ea123439593
jjaneyxx <hmyeonge@gmail.com>
2024-08-26 23:32:13 +0900

    Feat: 상황에 따라 보여지는 그림 바꾸기, Style: 전체적인 디자인 수정

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
 let resetButton = document.getElementById("reset-button");
 let chanceArea = document.getElementById("chance-area");
+let gameImage = document.getElementById("game-image");
 
 let chance = 5; // 기회
 let gameOver = false;
@@ -30,16 +31,18 @@ function pickRandomNum() {
 function play() {
   let userValue = userInput.value;
 
-  // 유저가 1보다 작거나 10보다 큰 값을 입력하는 경우
+  // 유효성 검사 1. 유저가 1보다 작거나 10보다 큰 값을 입력하는 경우
   if (userValue < 1 || userValue > 10) {
     resultArea.textContent = "The number should be between 1 and 10";
+    gameImage.src = "./images/huh.gif";
     return; // 함수를 종료
   }
 
-  // 입력한 값이 배열에 있는지 확인
+  // 유효성 검사 2. 입력한 값이 배열에 있는지 확인
   if (userInputArr.includes(userValue)) {
     // 이미 이전에 입력한 값을 또 입력함
     resultArea.textContent = "Enter a number different from the previous input";
+    gameImage.src = "./images/huh.gif";
     return;
   }
 
@@ -51,13 +54,16 @@ function play() {
   chanceArea.textContent = `${chance} chances left`;
 
   if (userValue < computerNum) {
-    resultArea.textContent = "UP !!!!! ⬆️";
+    resultArea.textContent = "UP ⬆️⬆️⬆️";
+    gameImage.src = "./images/up.gif";
   } else if (userValue > computerNum) {
-    resultArea.textContent = "Down !!!! ⬇️";
+    resultArea.textContent = "DOWN ⬇️⬇️⬇️";
+    gameImage.src = "./images/down.gif";
   } else {
-    resultArea.textContent = "Correct !!! 🥳";
-    playButton.disabled = true;
+    resultArea.textContent = "CORRECT !!! 🥳";
+    gameImage.src = "./images/correct.gif";
     chanceArea.textContent = `The answer was ${computerNum}`;
+    playButton.disabled = true;
   }
 
   if (chance < 1) {
@@ -68,16 +74,18 @@ function play() {
   if (gameOver) {
     playButton.disabled = true;
     resultArea.textContent = "Used up all your chances, sorry !😓";
+    gameImage.src = "./images/lose.gif";
     chanceArea.textContent = `The answer was ${computerNum}`;
   }
 }
 
 function reset() {
   userInput.value = ""; // 입력했던 번호들 초기화
-  pickRandomNum(); // 새로운 번호 생성
-  resultArea.textContent = "Let the games begin !";
-  chance = 5;
-  chanceArea.textContent = "You've got 5 chances";
   playButton.disabled = false;
+  resultArea.textContent = "Let the Game begin ~!";
+  chance = 5;
+  chanceArea.textContent = "Chances : 5";
   userInputArr = []; // 입력값이 담긴 배열도 초기화
+  pickRandomNum(); // 새로운 번호 생성
+  gameImage.src = "./images/main.gif";
 }
